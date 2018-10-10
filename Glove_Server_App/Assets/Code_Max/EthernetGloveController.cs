1aae523b2b382c2e83ae29df3264425e58f0974a
Max Heimbrock <max.heimbrock@gmx.de>
2018-10-10 10:53:03 +0200

    Trying to switch to Ethernet connection

---
+++ Glove_Server_App/Assets/Code_Max/EthernetGloveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthernetGloveController : MonoBehaviour {
+
+    public TrackingData TrD;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
