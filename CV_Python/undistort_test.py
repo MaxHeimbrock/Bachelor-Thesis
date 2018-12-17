364cbb527d486fa0116cc3390db526089f14739c
MaxHeimbrock <max.heimbrock@gmx.de>
2018-12-17 10:47:21 +0100

    laptop py

--- CV_Python/undistort_test.py
+++ CV_Python/undistort_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import math
 
 def is_similar(image1, image2):
     return image1.shape == image2.shape and not(np.bitwise_xor(image1,image2).any())
@@ -61,32 +62,75 @@ while (cap.isOpened()):
             #cv2.imshow('undist_frame', dst)
 
             #print(is_similar(frame, dst))
+
             imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-            im2, contours, hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(im2, contours, -1, (0, 255, 0), 3)
-            cv2.imshow('im2', im2)
-            # create hull array for convex hull points
-            hull = []
+            image, contours, hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            # calculate points for each contour
-            for i in range(len(contours)):
-                # creating convex hull object for each contour
-                hull.append(cv2.convexHull(contours[i], False))
+            crop_image = frame[100:300, 100:300]
 
-            # create an empty black image
-            drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+            try:
+                # Find contour with maximum area
+                contour = max(contours, key=lambda x: cv2.contourArea(x))
 
-            # draw contours and hull points
-            for i in range(len(contours)):
-                color_contours = (0, 255, 0)  # green - color for contours
-                color = (255, 0, 0)  # blue - color for convex hull
-                # draw ith contour
-                cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
-                # draw ith convex hull object
-                cv2.drawContours(drawing, hull, i, color, 1, 8)
+                # Create bounding rectangle around the contour
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(crop_image, (x, y), (x + w, y + h), (0, 0, 255), 0)
 
-            cv2.imshow('im2', drawing)
+                # Find convex hull
+                hull = cv2.convexHull(contour)
+
+                # Draw contour
+                drawing = np.zeros(crop_image.shape, np.uint8)
+                cv2.drawContours(drawing, [contour], -1, (0, 255, 0), 0)
+                cv2.drawContours(drawing, [hull], -1, (0, 0, 255), 0)
+
+                # Find convexity defects
+                hull = cv2.convexHull(contour, returnPoints=False)
+                defects = cv2.convexityDefects(contour, hull)
+
+                # Use cosine rule to find angle of the far point from the start and end point i.e. the convex points (the finger
+                # tips) for all defects
+                count_defects = 0
+
+                for i in range(defects.shape[0]):
+                    s, e, f, d = defects[i, 0]
+                    start = tuple(contour[s][0])
+                    end = tuple(contour[e][0])
+                    far = tuple(contour[f][0])
+
+                    a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                    b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                    c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                    angle = (math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 180) / 3.14
+
+                    # if angle > 90 draw a circle at the far point
+                    if angle <= 90:
+                        count_defects += 1
+                        cv2.circle(crop_image, far, 1, [0, 0, 255], -1)
+
+                    cv2.line(crop_image, start, end, [0, 255, 0], 2)
+
+                # Print number of fingers
+                if count_defects == 0:
+                    cv2.putText(frame, "HELLO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                elif count_defects == 1:
+                    cv2.putText(frame, "TWO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                elif count_defects == 2:
+                    cv2.putText(frame, "THREE", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                elif count_defects == 3:
+                    cv2.putText(frame, "FOUR", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                elif count_defects == 4:
+                    cv2.putText(frame, "FIVE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                else:
+                    pass
+            except:
+                pass
+
+                # Show required images
+            cv2.imshow("Gesture", frame)
+            all_image = np.hstack((drawing, crop_image))
+            cv2.imshow('Contours', all_image)
 
             cv2.waitKey(0)
             break
